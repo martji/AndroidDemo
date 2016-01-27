9b09cac2e25aca1de1a0e11069883aeb8a55229f
martji <928499mx>
2016-01-27 11:57:46 +0800

    MOD: fix launch mode

--- app/src/main/java/com/example/maguoqing/androiddemo/activity/NotificationActivity.java
+++ app/src/main/java/com/example/maguoqing/androiddemo/activity/NotificationActivity.java
@@ -1,8 +1,12 @@
 package com.example.maguoqing.androiddemo.activity;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +20,8 @@ public class NotificationActivity extends BaseActivity {
 
     @ViewId(R.id.btn_simple_notification)
     private Button btnSimpleNotification;
+    @ViewId(R.id.btn_back_notification)
+    private Button btnBackNotification;
     @ViewId(R.id.btn_clear_notification)
     private Button btnClearNotification;
     @ViewId(R.id.btn_progress_notification)
@@ -38,6 +44,9 @@ public class NotificationActivity extends BaseActivity {
             case R.id.btn_simple_notification:
                 showSimpleNotification();
                 break;
+            case R.id.btn_back_notification:
+                showNotification();
+                break;
             case R.id.btn_progress_notification:
                 showProgressNotification();
                 break;
@@ -55,6 +64,7 @@ public class NotificationActivity extends BaseActivity {
     @Override
     protected void setListeners() {
         btnSimpleNotification.setOnClickListener(this);
+        btnBackNotification.setOnClickListener(this);
         btnClearNotification.setOnClickListener(this);
         btnProgressNotification.setOnClickListener(this);
         btnStartDownload.setOnClickListener(this);
@@ -81,7 +91,39 @@ public class NotificationActivity extends BaseActivity {
         mBuilder.setContentTitle("Hello World")
                 .setContentText("This is a simple notification.")
                 .setSmallIcon(R.drawable.ic_launcher);
-        mNotificationManager.notify(0, mBuilder.build());
+        Intent resultIntent = new Intent(this, NotificationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(pendingIntent);
+        Notification notification = mBuilder.build();
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        mNotificationManager.notify(0, notification);
+    }
+
+    public void showNotification() {
+        mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setContentTitle("Hello World")
+                .setContentText("This is a simple notification.")
+                .setSmallIcon(R.drawable.ic_launcher);
+        Intent resultIntent = new Intent(this, NotificationActivity.class);
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(NotificationActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent pendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+//        Intent backIntent = new Intent(this, MainActivity.class);
+//        Intent intent = new Intent(this, NotificationActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+//        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+//                new Intent[] {backIntent, intent}, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(pendingIntent);
+        Notification notification = mBuilder.build();
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        mNotificationManager.notify(0, notification);
     }
 
     public void showProgressNotification() {
